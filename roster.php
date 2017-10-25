3514cf3c451f9b5dfd5ab5f304ed5da8fd30c236
choigunho <choiguenho@gmail.com>
2017-10-25 20:54:34 +0900

    modify main page

--- roster.php
+++ roster.php
@@ -3,7 +3,7 @@
     $result = mysqli_query($conn, "select * from roster order by BACK_NUM");
 
 
-    echo "<div class='container'>";
+//    echo "<div class='container'>";
     echo "<div data-role='content'>";
     echo "<div class='table-responsive'>";
     echo "<table class='table table-hover'> <tr> <th style='width:10%'>#</th> <th style='width:20%'></th> <th>Name</th> <th style='width:15%'>B/T</th> <th style='width:15%'>Age</th> </tr>";
@@ -29,7 +29,7 @@
     echo "</table>";
     echo "</div>";
     echo "</div>";
-    echo "</div>";
+//    echo "</div>";
 
     mysqli_close($conn);
 ?>
